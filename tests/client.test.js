13f45b034134e807d9aa6f9912cfe18002855381
Nicholas Boutboul <boutboulnicholas@gmail.com>
2018-07-10 04:14:32 -0700

    wrote out react tests, now i have to fill in the blanks, wrote mock payloads for nock

--- tests/client.test.js
+++ tests/client.test.js
@@ -2,56 +2,192 @@
 
 import React              from 'react';
 import { expect, assert } from 'chai';
+import { spy }            from 'sinon';
+import nock               from 'nock';
+
 import Enzyme, { mount }  from 'enzyme';
 import Adapter            from 'enzyme-adapter-react-16';
-import { spy }            from 'sinon';
-
-import ChatBox from '../client/src/components/chatBox';
-
 Enzyme.configure({ adapter: new Adapter() });
 
-spy(ChatBox.prototype, 'componentDidMount');
+import Room from '../client/src/containers/Room';
+import Home from '../client/src/containers/Home';
 
-describe('Testing chatbox component', () => {
-	const defaultPropsChatbox = {
-		err: false,
-		onChange: console.log,
-		sendMsg: console.log,
-		messages: [
-			{
-				"_id": "5b40eac2433afe0269d585da",
-				"text": "testUser has joined the room...",
-				"owner": {
-					"_id": "5b40eab3433afe0269d585d4",
-					"username": "testUser",
-				},
-				"type": "joined",
-			},
-			{
-				"_id": "5b40eac2433afe0269d585da",
-				"text": "this is a test :computer: $aapl $appl",
-				"owner": {
-					"_id": "5b40eab3433afe0269d585d4",
-					"username": "testUser",
-				},
-				"type": "text",
-			},
-		],
-		chatMsgValue: "",
-		user: {},
-		rId: "12345678",
-	};
+import Signup     from '../../components/signup';
+import RoomList   from '../../components/roomList';
+import RoomCreate from '../../components/roomCreate';
+import RoomJoin   from '../../components/roomJoin';
+import ChatBox    from '../client/src/components/chatBox';
 
-	it('calls componentDidMount without errors', done => {
-		const wrapper = mount(<ChatBox {...defaultPropsChatbox} />);
-		expect(ChatBox.prototype.componentDidMount.calledOnce).to.equal(true);
-		done();
+
+describe('testing full render', () => {
+	// before(() => {
+		
+	// });
+
+	// after(done => {
+		
+	// });
+
+	describe('Home container', () => {
+		it('updates value(in state) when text is input to user register', done => {
+
+		});
+
+		it('on click to create user, makes a call to API, re-renders once call is done', done => {
+
+		});
+
+		it('user sees populated homepage now that token is stored and sent with every api request', done => {
+
+		});
+
+		it('clicking on create rooms calls create room func', done => {
+
+		});
+
+		it('clicking on join room with text in input calls join room func', done => {
+
+		});
 	});
 
-	it('', done => {
+	describe('Room container', () => {
+		it('test if state msg array is same size as props input array', done => {
 
-	})
+		});
+
+		it('test if msg bubbles have been rendered', done => {
+
+		});
+
+		it('clicking on send will call send msg call with msg', done => {
+
+		});
+
+		it('back button will call backBtn function', done => {
+
+		});
+
+		it('room and room id rendered properly', done => {
+
+		});
+	});
 
 });
 
+// in charge of testing component methods and what not
+describe('testing individual components', () => {
+
+	describe('roomList component', () => {
+		it('renders \'no rooms\' when empy array sent in props', done => {
+
+		});
+		it('renders list of rooms that where in array provided', done => {
+
+		});
+	});
+
+	describe('chatBox component', () => {
+		it('parses msg with parseMsg functuon', done => {
+
+		});
+
+		it('counts stock in msg with parseForAllStocks function', done => {
+
+		});
+	});
+
+});
+
+// takes care of how app handles bad requests
+describe('testing for errors', () => {
+	describe('components render err msg when err prop is passed true', () => {
+		it('signup', done => {
+
+		});
+
+		it('roomList', done => {
+
+		});
+
+		it('roomJoin', done => {
+
+		});
+
+		it('roomCreate', done => {
+
+		});
+
+		it('chatBox', done => {
+			// will have to load stock api to do both
+		})
+	});
+
+	describe('nothing happens when nothing is in input box', () => {
+		it('signup', done => {
+
+		});
+
+		it('roomJoin', done => {
+
+		});
+
+		it('chatBox', done => {
+			
+		})
+	});
+});
+
 // WORK IN PROGRESS
+
+
+// describe('testing chatbox component', () => {
+// 	before(() => {
+// 		spy(ChatBox.prototype, 'componentDidMount');
+// 	});
+
+// 	after(done => {
+// 		done();
+// 	});
+
+// 	const defaultPropsChatbox = {
+// 		err: false,
+// 		onChange: console.log,
+// 		sendMsg: console.log,
+// 		messages: [
+// 			{
+// 				"_id": "5b40eac2433afe0269d585da",
+// 				"text": "testUser has joined the room...",
+// 				"owner": {
+// 					"_id": "5b40eab3433afe0269d585d4",
+// 					"username": "testUser",
+// 				},
+// 				"type": "joined",
+// 			},
+// 			{
+// 				"_id": "5b40eac2433afe0269d585da",
+// 				"text": "this is a test :computer: $aapl $appl",
+// 				"owner": {
+// 					"_id": "5b40eab3433afe0269d585d4",
+// 					"username": "testUser",
+// 				},
+// 				"type": "text",
+// 			},
+// 		],
+// 		chatMsgValue: "",
+// 		// user: {},
+// 		// rId: "12345678",
+// 	};
+// 	const stockMsgTest = "Hello $aapl this is not a stock $lolxd"
+// 	const emojiMsgTest = "stop...:hammer: time!"
+
+// 	it('calls componentDidMount without errors', done => {
+// 		const wrapper = mount(<ChatBox {...defaultPropsChatbox} />);
+// 		expect(ChatBox.prototype.componentDidMount.calledOnce).to.equal(true);
+// 		done();
+// 	});
+
+// 	// it('Parsing functions work', done => {
+// 	// 	const wrapper = mount(<ChatBox {...defaultPropsChatbox} />);
+// 	// })
+
+// });
---
+++ tests/mockPayloads.js
@@ -0,0 +1,102 @@
+// This is a new concept to me and I just found out about it and its super cool
+// and i really want to test this out
+
+// the goal of this is to make an enpoint for each call, since there is not many
+// it wont be hard to populate, run the app normaly with the mock data and make
+// sure that everything is tested properly with the fake data, this helps out
+// a lot because i was strugling with figuring out how to run the server
+// simuntaniouly and this also helps a lot with knowing what to test
+// for this I will be using nock
+
+// default error payload
+const defaultErr = {
+	success: false,
+}
+// POST api/create/user
+const createUser = {
+	success: true,
+	token: "dummy token",
+	user: {
+		username: "testUsertheReturn",
+		roomsJoined: [],
+	},
+};
+// GET api/user
+const getUserNoRooms = {
+	success: true,
+	user: {
+		username: "testUser",
+		roomsJoined: [],
+	},
+};
+const getUser = {
+	success: true,
+	user: {
+		username: "testUser",
+		roomsJoined: [
+			{
+				isActive: true,
+				_id: "5b40eabf433afe0269d585d5",
+				name: "Sheerraver Saver",
+				link: "6c47f6e4",
+				owner: "5b40eab3433afe0269d585d4",
+			},
+			{
+				isActive: true,
+				_id: "5b40eabf433afe0269d585d5",
+				name: "Sheerraver Saver",
+				link: "6c47f6e4",
+				owner: "5b40eab3433afe0269d585d4",
+			},
+		],
+	},
+};
+// GET api/create/room
+const createRoom = {
+	success: true,
+	roomLink: "7cdfd782",
+};
+// GET api/room/:rId
+const getRoom = {
+	success: true,
+	room: {
+		isActive: true,
+		_id: "5b40eac2433afe0269d585d9",
+		name: "Pondsnout Sage",
+		link: "7cdfd782",
+		owner: "5b40eab3433afe0269d585d4",
+		messages: [
+			{
+				_id: "5b40eac2433afe0269d585da",
+				text: "testUser has joined the room...",
+				owner : {
+					 _id : "5b40eab3433afe0269d585d4",
+					 username : "testUser",
+				},
+				 type : "joined",
+				 createdAt : "2018-07-07T16:30:58.184Z",
+				 updatedAt : "2018-07-07T16:30:58.184Z".
+			},
+			{
+				 _id : "5b40eac2433afe0269d585da",
+				 text : "this is a test :computer: $aapl $appl",
+				 owner : {
+				 _id : "5b40eab3433afe0269d585d4",
+				 username : "testUser",
+				},
+				 type : "text",
+			},
+		],
+		 createdAt : "2018-07-07T16:30:58.184Z",
+		 updatedAt : "2018-07-07T16:30:58.184Z",
+		 __v : 0,
+	},
+	 user : {
+		 username : "testUser",
+		 _id : "5b40eab3433afe0269d585d4",
+	}
+};
+// POST api/create/msg/:rId
+const createMsg = {
+	success: true,
+};
